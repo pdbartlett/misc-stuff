10c429d04c949d2dc8a9b9971284e9e32c1a56b1
Paul Bartlett <pdbartlett@gmail.com>
2013-11-10 23:03:25 +0000

    More work...

--- web/hfc/config.rb
+++ web/hfc/config.rb
@@ -1,3 +1,5 @@
 set :css_dir, 'stylesheets'
 set :images_dir, 'images'
 set :relative_links, true
+
+activate :livereload
--- web/hfc/helpers/site_helpers.rb
+++ web/hfc/helpers/site_helpers.rb
@@ -2,5 +2,11 @@ module SiteHelpers
   def s_img(fragment)
     image_tag fragment
   end
+  def s_link(link_text, fragment)
+    link_to link_text, data.site.link_root + fragment
+  end
+  def s_link_block(fragment, &block)
+    link_to data.site.link_root + fragment, &block
+  end
 end
     
