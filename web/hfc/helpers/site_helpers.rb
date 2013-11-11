07b8c28de84fde84b92120993a67dd495a8166ac
Paul Bartlett <pdbartlett@gmail.com>
2013-11-11 09:53:16 +0000

    Rest of links and next image section

--- web/hfc/helpers/site_helpers.rb
+++ web/hfc/helpers/site_helpers.rb
@@ -3,7 +3,11 @@ module SiteHelpers
     image_tag fragment
   end
   def s_link(link_text, fragment)
-    link_to link_text, data.site.link_root + fragment
+    if fragment.start_with? "http"
+      link_to link_text, fragment
+    else
+      link_to link_text, data.site.link_root + fragment
+    end
   end
   def s_link_block(fragment, &block)
     link_to data.site.link_root + fragment, &block
