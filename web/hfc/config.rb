89f65236e2db1d24b3b8c0628e4a42b141d6dbc1
Paul Bartlett <pdbartlett@gmail.com>
2013-11-13 17:41:39 +0000

    Add minification

--- web/hfc/config.rb
+++ web/hfc/config.rb
@@ -3,3 +3,9 @@ set :images_dir, 'images'
 set :relative_links, true
 
 activate :livereload
+
+configure :build do
+  activate :minify_css
+  activate :minify_html
+  activate :minify_javascript
+end
