2b6ea87f3f29b43da34e8934eb6dd8356227603f
Paul Bartlett <pdbartlett@gmail.com>
2013-11-13 08:59:06 +0000

    Extract a couple more partials and convert header to YAML-based definition

--- web/hfc/helpers/site_helpers.rb
+++ web/hfc/helpers/site_helpers.rb
@@ -1,9 +1,10 @@
 module SiteHelpers
   def s_img(fragment)
-    if data.site.image_root.empty?
+    root = data.site.image_root
+    if root.nil? or root.empty?
       image_tag fragment
     else
-      image_tag data.site.image_root + fragment
+      image_tag root + fragment
     end
   end
   def s_link(link_text, fragment)
@@ -15,6 +16,20 @@ module SiteHelpers
   end
   def s_link_block(fragment, &block)
     link_to data.site.link_root + fragment, &block
+  end
+  def s_section(elements)
+    rendered = elements.map do |element|
+      if element.type == "image"
+        if element.link.nil?
+          s_img element.src
+        else
+          s_link_block element.link do
+            s_img element.src
+          end
+        end
+      end
+    end
+    rendered.join ""
   end
 end
     
