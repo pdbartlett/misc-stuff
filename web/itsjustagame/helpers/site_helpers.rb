bbf1745329ed232139577c1d9aed4621eedcee47
Paul Bartlett <pdbartlett@gmail.com>
2013-11-18 14:27:16 +0000

    Beginnings of related content

--- web/itsjustagame/helpers/site_helpers.rb
+++ web/itsjustagame/helpers/site_helpers.rb
@@ -1,7 +1,54 @@
 module SiteHelpers
+  class StartWithChecker
+    def initialize(prefix)
+      @prefix = prefix
+    end
+    def ===(str)
+      (not str.nil?) and str.start_with? @prefix
+    end
+  end
+
+  def start_with(prefix)
+    StartWithChecker.new prefix
+  end
+  
+  def articles_for(page)
+    elements = []
+    case page.url
+    when "/"
+      title = "Featured content"
+      matches = [] # TODO: implement!
+    when start_with("/articles/")
+      title = "Related articles"
+      matches = [] # TODO: implement!
+    when start_with("/sections/")
+      title = "Articles"
+      matches = section_query page.data["title"].downcase
+    else
+      title = ""
+      matches = []
+    end
+    if not title.empty?
+      elements << (content_tag :h2, title)
+      if matches.empty?
+        elements << (content_tag :span, "None found.")
+      else
+        snippets = matches.map do |article|
+          partial :snippet, :locals => { :article => article }
+        end
+        elements.concat snippets
+      end
+    end
+    elements.join ""
+  end
+  
+  def section_query(name)
+    sitemap.where(:section.equal => name).all
+  end
+  
   def sections
-    sitemap.resources.keep_if do |r|
-      r.request_path.start_with? "sections/"
+    sitemap.resources.select do |r|
+      r.url.start_with? "/sections/"
     end
   end
   
--- web/itsjustagame/source/articles/test.html.md
+++ web/itsjustagame/source/articles/test.html.md
@@ -1,4 +1,5 @@
 ---
 title: Testing
+section: meta
 ---
 This is a test article written using *MarkDown*.
