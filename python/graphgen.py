c20e8ee1bcdacf4faa04cad39e811361ea36b01b
Paul Bartlett <pdbartlett@gmail.com>
2020-08-29 17:06:09 +0100

    Use itertools.count()

--- python/graphgen.py
+++ python/graphgen.py
@@ -1,4 +1,5 @@
 import csv
+import itertools
 import os
 import sys
 
@@ -16,19 +17,17 @@ print('graph', os.path.splitext(os.path.basename(sys.argv[1]))[0], '{')
 print('layout=sfdp; overlap=false; splines=true; ratio=0.707')
 print('node [ shape=egg fontname=Noteworthy penwidth=0 ]')
 print('edge [ penwidth=2 ]')
-c = 0
 ids = {}
+c = itertools.count()
 for n, col in colours.items():
-    id = 'node%d' % (c, )
+    id = 'node' + str(next(c))
     ids[n] = id
-    c += 1
     bri = sum(list(int(col[i:i+2], 16) for i in (1, 3, 5)))
     fc = 'white' if bri < 420 else 'black'
     print('%s [ label=<<b>%s</b>> style=filled fillcolor="%s" fontcolor=%s fontsize=20 ]' % (id, n, col, fc))
 for ch, p in parents.items():
-    id = 'node%d' % (c, )
+    id = 'node' + str(next(c))
     ids[ch] = id
-    c += 1
     print('%s [ label=<<b>%s</b>> shape=none margin=0 width=0 height=0 fontsize=12 ]' % (id, ch))
     print('%s -- %s [ color="%s" ]' % (ids[p[0]], ids[ch], colours[p[0]]))
     print('%s -- %s [ color="%s" ]' % (ids[p[1]], ids[ch], colours[p[1]]))
