5f470ea9620173c376a20d26f7c909d98689ce81
Paul Bartlett <pdbartlett@gmail.com>
2020-08-31 13:09:17 +0100

    Decompose functions and templatize.

--- cpp/pipette/main.cc
+++ cpp/pipette/main.cc
@@ -1,17 +1,28 @@
+#include <functional>
 #include <iostream>
 
 #include "absl/memory/memory.h"
 
 #include "pipette.h"
 
+template<typename T>
+void print(T t) {
+  std::cout << t << std::endl;
+}
+
+template<typename T>
+void run(Source<T>* src, std::function<void(T)> f) {
+  T t;
+  while (src->fetch(&t)) {
+    f(t);
+  }
+}
+
 int main(int argc, char* argv[]) {
   std::cout << "Test App" << std::endl;
   int data[] = {1, 3, 5, 7, 9};
   auto src =
       absl::make_unique<ArraySource<int>>(data, sizeof(data)/sizeof(data[0]));
-  int n;
-  while (src->fetch(&n)) {
-    std::cout << n << std::endl;
-  }
+  run<int>(src.get(), print<int>);
   return 0;
 }
