00886aee8d2c609e996c6fbf6b965d203b71cad5
Paul Bartlett <pdbartlett@gmail.com>
2020-09-04 17:37:42 +0100

    Start with college.csv exercise

--- python/islp/02statlearn.py
+++ python/islp/02statlearn.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 
-print('Numpy\n-----')
+print('Basic\n=====')
+
+print('\nNumpy\n-----')
 
 print(np.array([1,3,2,5]))
 
@@ -16,8 +18,9 @@ print('\nPandas\n------')
 
 print(pd.Series([1,2,3,4,5]))
 
-print('\nExercises\n---------')
+print('\nExercises\n=========')
 
+print('\nKNN\n---')
 d = {'X1': [ 0,   2,   0,   0,  -1,   1 ],
      'X2': [ 3,   0,   1,   1,   0,   1 ],
      'X3': [ 0,   0,   3,   2,   1,   1 ],
@@ -29,3 +32,7 @@ df = df.sort_values(by='dist')
 print('K=1 =>', df.head(1).Y.to_numpy()[0])
 print('K=3 =>', df.head(3).groupby('Y').count().sort_values(by='dist', # arbitrary
     ascending=False).index.values[0])
+
+print('\nCollege.csv\n-----------')
+df = pd.read_csv('College.csv')
+print(df)
