2c995e8a72c356d23282fbc8f708885980e9d767
Paul Bartlett <pdbartlett@gmail.com>
2020-09-04 17:25:16 +0100

    Add comment.

--- python/islp/02statlearn.py
+++ python/islp/02statlearn.py
@@ -23,9 +23,9 @@ d = {'X1': [ 0,   2,   0,   0,  -1,   1 ],
      'X3': [ 0,   0,   3,   2,   1,   1 ],
      'Y':  ['R', 'R', 'R', 'G', 'G', 'R']}
 df = pd.DataFrame(data = d)
+print(df)
 df = df.assign(dist = (df.X1**2 + df.X2**2 + df.X3**2)**(0.5))
 df = df.sort_values(by='dist')
-print(df)
 print('K=1 =>', df.head(1).Y.to_numpy()[0])
-print('K=3 =>', df.head(3).groupby('Y').count().sort_values(
-    by='dist', ascending=False).index.values[0])
+print('K=3 =>', df.head(3).groupby('Y').count().sort_values(by='dist', # arbitrary
+    ascending=False).index.values[0])
