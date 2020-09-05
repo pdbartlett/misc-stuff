e6dcb27d7a9a258b8c49bb31d8541a27bb99c18c
Paul Bartlett <pdbartlett@gmail.com>
2020-09-05 16:16:16 +0100

    Add matplotlib versions of plotting examples.

--- python/islp/02statlearn.py
+++ python/islp/02statlearn.py
@@ -1,5 +1,10 @@
+import math
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
+from matplotlib import cm
 
 print('Basic\n=====')
 
@@ -13,6 +18,42 @@ y = np.array([1,4,3])
 print(x, x.shape, x.size)
 print(y, x.shape, x.size)
 print(x + y)
+
+m = np.array([[1, 2], [3, 4]])
+print(m)
+print(m**(0.5))
+
+x = np.random.normal(size=50)
+y = x + np.random.normal(size=50, loc=50, scale=0.1)
+r = np.corrcoef(x, y)[0][1]
+print(r)
+a, b = np.polyfit(x, y, deg=1)
+y_est = a * x + b
+plt.plot(x, y, 'r+')
+plt.plot(x, y_est)
+plt.show()
+
+x = np.arange(1, 11)
+print(x)
+
+a = np.linspace(-math.pi, math.pi)
+X, Y = np.meshgrid(a, a)
+Z = np.cos(Y) / (1 + X**2)
+plt.contour(X, Y, Z)
+plt.show()
+plt.contour(X, Y, Z, 45)
+plt.show()
+
+ZA = (Z - np.transpose(Z)) / 2
+plt.contour(X, Y, ZA, 15)
+plt.show()
+plt.imshow(ZA)
+plt.show()
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(X, Y, ZA, cmap=cm.coolwarm, lw=0, antialiased=False)
+plt.show()
 
 print('\nPandas\n------')
 
@@ -35,4 +76,4 @@ print('K=3 =>', df.head(3).groupby('Y').count().sort_values(by='dist', # arbitra
 
 print('\nCollege.csv\n-----------')
 df = pd.read_csv('College.csv')
-print(df)
+#print(df)
