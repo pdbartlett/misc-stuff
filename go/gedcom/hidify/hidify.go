0f77bcbf7ca956c7b68e003d543e2241c0f03d73
Paul Bartlett <pdbartlett@gmail.com>
2021-04-12 15:26:12 +0100

    Add file open/create logic.

--- go/gedcom/hidify/hidify.go
+++ go/gedcom/hidify/hidify.go
@@ -7,8 +7,17 @@ import (
 )
 
 func main() {
-  if len(os.Args) != 2 {
-    this, _ := os.Executable()
-    log.Fatalf("Usage: %s input-file output-file", path.Base(this))
+  if len(os.Args) != 3 {
+    log.Fatalf("Usage: %s input-file output-file", path.Base(os.Args[0]))
   }
+  i, err := os.Open(os.Args[1])
+  if err != nil {
+    log.Fatalf("Cannot open %q for reading", os.Args[1])
+  }
+  defer i.Close()
+  o, err := os.Create(os.Args[2])
+  if err != nil {
+    log.Fatalf("Cannot create new file %q", os.Args[2])
+  }
+  defer o.Close()
 }
