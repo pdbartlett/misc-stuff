30dbd581dda4ee4c1e9545117c29b18baeef7a16
Paul Bartlett <pdbartlett@gmail.com>
2022-09-25 00:31:15 +0100

    A couple of improvements to wordle.sh

--- other/wordle.sh
+++ other/wordle.sh
@@ -1,16 +1,25 @@
 #/bin/sh
-PATT=$1
+set -o errexit
+set -o nounset
+
+GREEN=$1
 YELL=$2
 GREY=$3
+PATT=${4:-}
 
-NPAT=$(echo ${PATT} | sed s/^/\^/ | sed s/$/\$/ | sed s/\\./\[\^${GREY}\]/g)
-grep -i ${NPAT} /usr/share/dict/words >wordle.tmp
+REGEX=$(echo ${GREEN} | sed s/^/\^/ | sed s/$/\$/ | sed s/\\./\[\^${GREY}\]/g)
+grep -i ${REGEX} /usr/share/dict/words >wordle.tmp
 
 echo ${YELL} | grep -o . | while read ch
 do
   grep -i ${ch} wordle.tmp >wordle.tmp2
   mv wordle.tmp2 wordle.tmp
 done
+
+if [ -n "${PATT}" ]; then
+  grep -i "${PATT}" wordle.tmp >wordle.tmp2
+  mv wordle.tmp2 wordle.tmp
+fi
 
 cat wordle.tmp
 rm wordle.tmp
