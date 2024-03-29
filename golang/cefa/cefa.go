a58d8692bb95743dffc717bfd880788a7b729b7b
Paul Bartlett <pdbartlett@gmail.com>
2024-03-29 20:24:07 +0000

    Fix extra space

--- golang/cefa/cefa.go
+++ golang/cefa/cefa.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-  for err := range  new(signifo.Server).RunAsync(":2305") {
+  for err := range new(signifo.Server).RunAsync(":2305") {
     log.Printf("ERROR: %v", err)
   }
   log.Println("Exiting")
